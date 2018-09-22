fd7cd71d31e3a36d65e8ba315203e524b8b70886
Desmond Henaghen <deshenaghen@tuta.io>
2018-09-22 19:36:09 +0100

    Can now navigate from any page.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NgsRevealConfig} from 'ng-scrollreveal';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,21 @@ import {NgsRevealConfig} from 'ng-scrollreveal';
 export class AppComponent {
   title = 'app';
 
-  constructor(config: NgsRevealConfig) {
+  constructor(config: NgsRevealConfig, private router: Router) {
     // customize default values of ng-scrollreveal directives used by this component tree
     config.duration = 500;
     config.easing = 'cubic-bezier(0.645, 0.045, 0.355, 1)';
   }
 
-  scrollToBlock(id: String, mobile: boolean=false): void {
-    if (mobile) document.getElementById("nav-toggle").click();
+  scrollToBlock(id: String, mobile: boolean= false): void {
+    if (this.router.url !== '/') { this.navigateToMainPage(id); }
+
+    if (mobile) { document.getElementById('nav-toggle').click(); }
     const element = document.querySelector('#' + id);
     if (element) { element.scrollIntoView({ behavior: 'smooth', block: 'start' }); }
+  }
+
+  private navigateToMainPage(id: String): void {
+    this.router.navigateByUrl('/');
   }
 }
