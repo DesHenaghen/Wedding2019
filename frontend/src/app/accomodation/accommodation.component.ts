2cce0d814e6b60b968a6de44497172250ab48a4d
Ira <irinacamilleri@gmail.com>
2018-09-22 21:25:41 +0100

    Accommodation details added.

--- frontend/src/app/accomodation/accommodation.component.ts
+++ frontend/src/app/accomodation/accommodation.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-accomodation',
   templateUrl: './accommodation.component.html',
-  styleUrls: ['./accommodation.component.css']
+  styleUrls: ['./accommodation.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AccommodationComponent implements OnInit {
 
