27610b064423d51373f2f8c9dfbd149d9d14f7c5
Desmond Henaghen <deshenaghen@tuta.io>
2018-09-23 20:05:08 +0100

    Added accommodation rates

--- frontend/src/app/angular-material/angular-material.module.ts
+++ frontend/src/app/angular-material/angular-material.module.ts
@@ -5,14 +5,14 @@ import {
   MatCheckboxModule, MatDialogModule,
   MatFormFieldModule, MatInputModule,
   MatSelectModule, MatSnackBarModule,
-  MatExpansionModule, MatTabsModule
+  MatExpansionModule, MatTabsModule, MatTableModule
 } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [MatInputModule, MatButtonModule, MatCheckboxModule, MatCardModule, MatFormFieldModule, MatSelectModule,
-    MatSnackBarModule, MatDialogModule, FormsModule, MatExpansionModule, MatTabsModule],
+    MatSnackBarModule, MatDialogModule, FormsModule, MatExpansionModule, MatTabsModule, MatTableModule],
   exports: [MatInputModule, MatButtonModule, MatCheckboxModule, MatCardModule, MatFormFieldModule, MatSelectModule,
-    MatSnackBarModule, MatDialogModule, FormsModule, MatExpansionModule, MatTabsModule],
+    MatSnackBarModule, MatDialogModule, FormsModule, MatExpansionModule, MatTabsModule, MatTableModule],
 })
 export class AngularMaterialModule { }
