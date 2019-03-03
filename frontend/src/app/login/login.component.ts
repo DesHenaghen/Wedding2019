f4e95b526a976fcdd57b54c923ee67a666b71d0b
HLayton <des_accounts@tuta.io>
2019-03-03 19:42:34 +0000

    non-prod build and all those files I missed out

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import {AuthService} from "../services/auth.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: []
+})
+export class LoginComponent {
+  public username: string;
+  public password: string;
+  public error: string;
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  public submit() {
+    this.auth.login(this.username, this.password)
+      .pipe(first())
+      .subscribe(
+        result => this.router.navigate(['admin']),
+        err => this.error = 'Could not authenticate'
+      );
+  }
+}
